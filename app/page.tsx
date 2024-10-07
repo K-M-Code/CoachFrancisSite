c7ef8f633c6b2068bcf1dd1761b3bbfbe4210669
kmcode <kmcode@kmpc>
2024-10-07 10:42:43 +0300

    Start Blog Integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Header from "@/components/Header";
 import HomeHero from "@/components/HomeHero";
 import HomeServices from "@/components/HomeServices";
-// import HomeBlog from "@/components/HomeBlog";
-// import Testimonials from "@/components/Testimonials";
+import HomeBlog from "@/components/HomeBlog";
+import Testimonials from "@/components/Testimonials";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -12,8 +12,8 @@ export default function Home() {
       <Header/>
       <HomeHero/>
       <HomeServices/>
-      {/* <Testimonials/>
-      <HomeBlog/> */}
+      <Testimonials/>
+      <HomeBlog/>
       <Footer/>
     </div>
   );
