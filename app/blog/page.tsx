e1632c0bd9b1e1402190c1f4c44ba239835b143c
kmcode <kmcode@kmpc>
2024-10-07 10:49:15 +0300

    Start Blog Integration

---
+++ app/blog/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+const BlogPage = () => {
+    return (
+        <>
+            <Header/>
+            
+        <div>
+            <h1>Blog Page</h1>
+        </div>
+            <Footer/>
+        </>
+    )
+}
+
+export default BlogPage
